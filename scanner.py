571a2dbd55971fe7dc6984db3953531a75119779
Belal Youssry Nada <bnada8796@gmail.com>
2026-08-18 12:38:31 +0300

    scanner

--- scanner.py
+++ scanner.py
@@ -41,7 +41,7 @@ while x ==True:
       print(f"3) installing requirements ⚒️{C['end']}")
       print("***************************")
       print(f"{C['green']}0) Exit tool 👋{C['end']}")
-      choose=input(f"{C['red']}fosciety@root {C['end']}")
+      choose=input(f"{C['red']} fosciety@root {C['end']}")
       if choose=="1":
          nmap()
          ban()
